a24b97bcab1371d269c3c6a7f4869129baaeaf45
RowDog <mrdiamonddirt@gmail.com>
2022-12-14 21:57:52 +0000

    styled watch face a little

--- web/src/components/watch-faces/default.tsx
+++ web/src/components/watch-faces/default.tsx
@@ -1,16 +1,22 @@
 import React, { useEffect } from "react";
 import { useTime } from "../../hooks/useTime";
 import { getDate } from "../../utils/date";
+import "./style.css";
+
+// add event listener for mouse click down and move mouse to the left
+// to change the watch face
+
+// First, we need to import the necessary TypeScript modules
 
 function DefaultWatchFace() {
     const { hour, minute, second } = useTime();
     return (
         <>
-            <div>DefaultWatchFace</div>
+            {/* <div>DefaultWatchFace</div> */}
             <div className="clock">
-                <div>{hour}</div>
-                <div>{minute}</div>
-                <div>{second}</div>
+                <div className="clockHour">{hour}</div>
+                <div className="clockMinute">{minute}</div>
+                <div className="clockSecond">{second}</div>
             </div>
         </>
     );
