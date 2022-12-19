2ffd70847cf4a76d44440f3e1f616771c3f4d2a9
RowDog <mrdiamonddirt@gmail.com>
2022-12-19 15:16:17 +0000

    added bars 😠 types

--- web/src/components/watch-faces/default.tsx
+++ web/src/components/watch-faces/default.tsx
@@ -3,8 +3,44 @@ import { useTime } from "../../hooks/useTime";
 import { getDate } from "../../utils/date";
 import "./style.css";
 
-// add event listener for mouse click down and move mouse to the left
-// to change the watch face
+// add event listener if you hover over the health food or water
+// display the current value of the health food or water within the bar
+
+// get classname of the bar that is being hovered over
+addEventListener("mouseover", function (event) {
+    // highlight the mouseover target
+    console.log(event.target.className);
+    var eventClass = event.target.className;
+    if (eventClass === "healthBarFill") {
+        console.log("healthBarFill");
+        displayBarValue(eventClass);
+    } else if (eventClass === "foodBarFill") {
+        console.log("foodBarFill");
+        displayBarValue(eventClass);
+    } else if (eventClass === "waterBarFill") {
+        console.log("waterBarFill");
+        displayBarValue(eventClass);
+    }
+});
+
+function displayBarValue(eventClass) {
+    // get the value of the bar
+    // console.log(eventClass);
+    var barValue = document.querySelector("." + eventClass);
+    console.log(barValue);
+    // console.log(barValue.innerHTML);
+    barValue.innerHTML = "100";
+    setTimeout(() => {
+        barValue.innerHTML = "";
+    }, 2000);
+}
+
+// var HealthBarValue = document.querySelector(".healthBarFill");
+
+// console.log(HealthBarValue);
+// setTimeout(() => {
+//     HealthBarValue.innerHTML = "";
+// }, 2000);
 
 // First, we need to import the necessary TypeScript modules
 
@@ -21,7 +57,7 @@ function DefaultWatchFace() {
             <div className="clockDisplayInfo">
                 Health
                 <div className="healthBar">
-                    <div className="healthBarFill"></div>
+                    <div className="healthBarFill" value="100"></div>
                 </div>
                 Food
                 <div className="foodBar">
